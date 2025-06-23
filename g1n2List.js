73dde2a617060619ef143f0202f4156a3f0dbd15
micknick911 <appiahm29@yahoo.com>
2025-06-23 18:37:19 +0000

    all

---
+++ g1n2List.js
@@ -0,0 +1,514 @@
+wordBank = `
+bat ham rat wax
+bag had ran yak
+bad jam rag zag
+can lap rap am
+cap lad sap an
+cab map sad at
+cat man Sam Ann
+dad mat tan lass
+fan nap tap mass
+fat pan tag pass
+gas pad tax
+hat quack wag
+has dab gab jab lab nab tab bad dad fad had lad mad pad sad tad bag gag hag lag mag nag rag sag tag wag bam dam ham jam ram yam ban can fan man pan ran tan van cap gap lap map nap rap Sap tap yap zap bat cat fat hat mat pat rat sat vat max tax wax pal gal gas mal pal bam cam ham jam ram sam tam zap bar tar car far par jar
+
+bib fix mill sip
+big hid miss sit
+bill hill mix six
+bin him nip Tim
+bit hit pig tin
+did if pill tip
+dig in pit vim
+dim it quick wig
+dip jig rib will
+fib kid rid win
+fig kill rig yip
+fill kiss rim zig-zag
+fin lid rip zip
+fit lip sin
+hin yin zit his nim lit bib zip vim wit hit pit pin sin min win bin tin wig big pig bid did hid kid lid rid big dig fig gig pig rig wig zig dim him rim bin din fin sin tin win dip hip lip nip rip sip tip zip bit fit hit kit sit wit fix mix six pix kin
+
+bob fog lot pot
+bog fox mob rob
+boss got mop rod
+box hod moss rot
+cob hog nod sob
+cot hop not sod
+dog hot off sop
+doll job on top
+Don log pod toss
+dot loss pop
+won cod lob nod bot log dot hop fog pod hog bob pop rot sot top mog job cot lot for cob gob job mob rob sob bog cog dog fog hog jog log bop cop hop mop pop top cot dot got hot jot lot not pot rot tot box fox pox rod sod mom god
+
+but duck hum pup
+bun dug jut rub
+bug fun jug rug
+bud fuss lug run
+bus gull mud sum
+cup gum muff sun
+cub gun mug tub
+cuff Gus muss up
+cut hug nut
+buy hub rub sub tub gus yum yup nub pub cud lug cum pus bud cub dud mud bug dug hug jug lug mug pug rug tug bum gum hum mum sum tum bun fun gun nun pun run sun but cut gut hut jut nut rut bus cup pup
+
+bed hen net ten
+beg jet peg vet
+bell let pen web
+bet leg pet well
+dell less red wet
+den let sell yell
+fed men set yes
+fell mess Ted yet
+get met tell
+meg vex fen mer per her fet ben ken gel beg yet gem zed ret sex bed fed red wed zed beg keg leg peg den hen men pen ten bet get jet let met net pet set vet wet web gem hem yes
+
+band hand pump
+bent hint pant
+bend jump quest duck
+belt just rest Jack
+best lend rust kick
+bump lent sent lock
+camp land send luck
+damp lamp silk neck
+desk hump next pick
+fast lift sand quick
+fist list tent quack
+felt lint test rock
+fond melt went sick
+fund must wind tack
+gift milk and
+tilt mist help
+gust mend its
+
+cash hush shed shop
+dish wish shelf shot
+fish shall ship shut
+
+than them this
+that then thus
+
+thick thud bath
+thin thump with
+
+chat chum fetch witch
+chin much latch thatch
+chill rich match
+chick such notch
+chop catch patch 
+chip ditch pit
+
+mechanic machine 
+
+want watch when whip
+was wash which whisk
+water what whiff wish
+
+bang ding wing song
+gang ping-pong king hung
+hang ring thing lung
+rang sing gong rung
+sang thing-a-ling long sung
+
+bank link sink chunk
+bunk mink wink thank
+dunk pink tank honk
+kink rink sank think
+
+black fling skin spill
+bland flip skip spin
+blend flit slam spit
+blink flock slap spot
+bliss flop slash spun
+block clung sled spunk
+blush glad slip stab
+clang gland slot stack
+clank glass slum stand
+clap plan slush stem
+clench plant smack step
+click plop smash stiff
+cliff plot smell still
+clinch pluck smelt stop
+cling plum smug stub
+clink plus snap stuck
+clip scalp snip stuff
+clock scan snob swam
+club scat snub swell
+flag scotch snuff swept
+flap scuff snug swift
+flash scum span swim
+flat sketch spank swing
+fled skid spat switch
+flesh skill spell
+
+brag crunch frump grin
+brand crust fresh prank
+brass drag French press
+brim drank Fred prim
+brick drop frog print
+bring drum frock track
+branch drink grand trap
+brush dress grass trick
+crab drunk grasp trim
+crash drip grant trip
+cramp drug gruff trot
+crack Fran grog truck
+crib Frank grip trend
+crop frost grunt
+
+ashcan dishpan hilltop sandbag
+backstop dogsled Hobbit shipment
+bedrock dustpan hubcap sunspot
+napkin eggnog kidnap tiptop
+blacktop endless hotdog traffic
+bobcat filmstrip landmass trashcan
+cashbox flapjack laptop upland 
+catfish gumdrop lesson sunset
+catnap handbag magnet sudden
+catnip handcuff Midland combat
+chestnut handstand nutmeg upon
+desktop helmet pocket
+
+ate tame bite tile rope
+bake tape dime tide sole
+cane blame dine wife stone
+cape flame dike wine scone
+cake plane fine wide spoke
+date stake fife slide slope
+daze glade dive size smoke
+fate snake file glide note
+fade chase five while tone
+gate safe hide white tote
+gaze paste hive drive those
+hate gave lime pride stole
+came case life prime mope
+haze fake like prize mole
+lake haste mine vote
+lame flake mite bone woke
+make save mile cone broke
+mane blaze nine cope drove
+made vase pike code probe
+mate taste pine dote 
+late waste pile dole cube 
+game brave quite dome duke
+wake brake kite globe dune
+name crate ride hole cute
+pale crave shine home tube
+quake craze side hope tune
+rate drape spike joke mule
+rake grape smile lone flute
+pane grade slime lode prune
+sake grave stile lope rule
+shame trade swine poke rude
+shake spine quote plume
+same here ripe rode brute
+take these time robe mete
+Steve here rede fete gene Pete complete
+eve squeeze sleeve sneeze
+
+bold colt most blind
+old jolt post find
+cold molt host grind
+fold bolt hind
+gold volt both kind
+hold mind
+mold toll child rind
+sold roll mild wind
+told wild 
+scold
+
+be he she go so
+he we the lo co 
+hi no flu
+
+bar dark mark scar
+barn far mart star
+car farm park start
+chart hard part tar
+charm jar spark warm
+darn lark smart
+
+born fork porch torn
+cord fort scorn torch
+cork horn scorch morn
+corn horse storm morning
+for pork stork
+
+clerk stir camper sitter
+fern third cutter starter
+jerk curb catcher swimmer
+her curl chopper sender
+herd burn dipper spinner
+term fur drummer better
+bird hurt helper never
+birth purr jumper after
+dirt urn marker under
+fir word farmer actor
+first world pitcher doctor
+girl work planner janitor
+sir worm runner visitor
+
+ail paid wail lay
+bail laid may
+brain pain bay pay
+fail rail clay pray
+gain rain day play
+grain sail gay ray
+jail tail gray say
+maid train hay sway
+mail wait jay way
+
+bee free need sleet
+beef freeze peep sweep
+beech fleet peek sweet
+beet green queen sheep
+deed greet reed wee
+deep heed see weed
+breeze heel seed week
+fee jeep seem weep
+feet keep seen
+feed keen sleep
+feel meet sleeve
+
+beat each reach threat
+beach leaf read thread
+beast leap real tread
+bean leave wealth
+cream lean bread weather
+cheat meal breast
+cheap least breath steak
+deal sea dead beak
+dream seat death great
+feast tea health bear
+east teach instead
+eat clean read
+
+cried lies tied priest
+cries lied tried relief
+dried pie believe
+dries pies brief thief
+fried spies chief yield
+lie tie grief belief 
+field
+
+army handy sleepy fly
+candy hilly thirty my
+corky healthy twenty ply
+daddy fairy silly sly
+dolly fifty wealthy try
+dusty messy pretty sky
+funny penny shy
+gummy puppy by why
+party rainy cry myself
+happy sunny dry
+
+boat load roast Joe
+boast loaf soapy toe
+coat road toes
+coach soap foe woe
+coast soak goes
+goat toad hoe
+float throat hoed
+
+bow tow yellow bowl
+throw blow show own 
+crow shown glow grow
+fellow snow grown follow
+growth hollow low shadow
+flow pillow row window
+slow willow
+
+gown growl how bow
+howl cow power crowd
+powder clown row crown
+now brown town down
+frown flower
+
+cloud mound round found
+out sound ground our
+shout house mouse scout
+loud pound
+
+you country young soul
+four
+
+boy toys boil soil
+boys oyster coin spoil
+joy oysters join toil
+joys oil going
+toy point
+
+boot moon stoop foolish
+booth roof spoon smooth
+bloom loose spool teaspoon
+coo room shoot noonday
+cool proof too toothbrush
+boost mood tool scooter
+droop gloom tooth papoose
+food noon troop tooting
+groom soon coolness school
+goose roost zoo
+doom stool cooler
+
+book good hood shook
+booklet foot look wool
+cook footstep looking wood
+crook goodness soot wooden
+brook hoof took woolen
+cooker hook stood footstool
+
+crawl hawk shawl faun
+crawling jaw thaw haul
+dawn law yawn fault
+draw lawn pause
+drawn paw cause
+fawn saw clause
+
+already bald salt mall
+almost false ball tall
+always halt call wall
+also malt fall
+
+blew flew news flue
+brew threw pew glue
+chew dew stew true
+crew few due
+drew mew blue hue
+grew new clue Sue
+
+a ajar around asleep
+about alike arouse astir
+adrift ahead apart awake
+afar amuse aside awhile
+
+careful full fullback put
+pull push full moon
+bull bush fulfill
+
+cent brace mice rice
+cell chance nice space
+cease decide niece slice
+center dance pace spice
+civil dunce place since
+cinder face peace twice
+cyclone fleece piece trace
+circus fence prance thence
+cinch France prince choice
+cigar hence princess voice
+acid ice pencil
+cistern lace price special
+ace mince race
+
+age page badge ridge
+barge plunge dodge smudge
+change rage edge wedge
+cage range fudge ginger
+engage sage hedge giraffe
+fringe stage lodge gist
+huge wage nudge giblet
+large urge pledge gyp
+lunge budge ledge gypsy
+hinge bridge judge gymnast
+
+bright might thigh caught rough
+blight night right daughter tough
+fight moonlight taught laugh
+fighter plight laughter
+flight sigh laughing
+high tight
+light slight
+
+bright might thigh caught rough
+blight night right daughter tough
+fight moonlight taught laugh
+fighter plight laughter
+flight sigh laughing
+high tight
+light slight
+
+knee chasten wrist lamb hour 
+kneel glisten wring limb honor
+knelt hasten wrote thumb honest
+knight listen wreck ghost
+knife often wrong calf
+knit soften half
+knot comb walk
+know wreath climb
+known wretch debt answer 
+knock write doubt sword dumb
+
+choose nose rise is
+chose pause tease has
+cheese pose these his
+ease praise those use
+because please wise
+noise rose as
+
+elephant prophet phonograph telegraph
+nephew pamphlet phone alphabet
+orphan photograph telephone phonics
+
+battle handle attention partition
+bundle puzzle action portion
+bottle scramble addition station
+buckle scribble affection section
+circle sprinkle invitation
+little struggle foundation expression
+middle tickle education impression
+pickle wiggle mention mission sample nation
+
+added ended painted waited
+acted folded planted
+counted landed printed
+crowded lighted rested
+
+aimed changed saved stayed
+burned filled rained turned
+called named rolled
+
+baked picked looked wished
+boxed hoped packed
+camped jumped pitched
+hitched liked stopped
+
+baker racer pony mural
+shady fever holy rural
+lady cedar over tyro
+paper hero clover tyrant
+caper legal donor
+taper regal solar dial
+favor before polar giant
+savor tidy sober trial
+vapor limy local friar
+wafer slimy focal pliant 
+maker viper vocal vial 
+taker biter total
+nasal libel oval poem
+fatal cider grocer brier
+natal spider oral
+label tiger open cruel
+halo vital tulip duel
+sago final Lucy fuel 
+pacer bony pupil
+
+do come again only where measure
+to done said are were pleasure
+today does carry every treasure
+together some could eight been
+two one would have buy
+who once give don’t
+into of any their your
+from many they very
+
+ find is not three and for it one to away funny jump play two big go little red up blue help look run we can here make said where come I me see yellow down in my the you all do no she well am eat now so went are four on soon what at get our that white ate good out there who be have please they will black he pretty this with brown into ran too yes but like ride under came must saw want did new say was
+ 
+ after fly how open then again from just over think an give know put walk any going let round were as had live some when ask has may stop by her of take could him old thank every his once them always does made tell why around don't many their wish because fast off these work been first or those would before five pull upon write best found read us your both gave right use buy goes sing very call green sit wash cold its sleep which after again
+ 
+ about eight if only ten better fall keep own today bring far kind pick together carry full laugh seven try clean got light shall warm cut grow long show done hold much six draw hot myself small drink hurt never start
+    above
+    
+apple children flower money sister baby Christmas game morning snow back coat garden mother song ball corn girl name squirrel bear cow goodbye nest stick bed day grass night street bell dog ground paper sun bird doll hand party table birthday door head picture thing boat duck hill pig time box egg home rabbit top boy eye horse rain toy bread farm house ring tree brother farmer kitty robin watch cake father leg Santa Claus water car feet letter school way cat fire man seed wind chair fish men sheep window chicken floor milk shoe wood
+    across
+`;
+
+var wordList = wordBank.trim().split(/[\s,\n]/).filter((word, index, self) => self.indexOf(word) === index);
